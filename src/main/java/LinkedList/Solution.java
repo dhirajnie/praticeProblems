93477b9eed3ac014b2d86b65dbb531174bcf702e
dhiraj kumar <dhirajnie@gmail.com>
2026-01-31 09:08:36 +0530

    sss

--- src/main/java/LinkedList/Solution.java
+++ src/main/java/LinkedList/Solution.java
@@ -1,6 +1,32 @@
 package LinkedList;
 
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
+
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+
+//    @Override
+//    public String toString() {
+//        return "Node{" +
+//                "val=" + val +
+//                ", next=" + next +
+//                ", random=" + random +
+//                '}';
+//    }
+}
+
 
 class ListNode {
     int val;
@@ -35,39 +61,92 @@ public class Solution {
     }
 
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1==null ){
+        if (l1 == null) {
             return l2;
-        }
-        else if(l2==null){
+        } else if (l2 == null) {
             return l1;
-        }
-        else{
-            if(l1.val>l2.val){
+        } else {
+            if (l1.val > l2.val) {
                 ListNode temp = l1;
-                l1=l2;
-                l2=temp;
+                l1 = l2;
+                l2 = temp;
             }
 
-            while (l1.next!=null && l2!=null){
-                if(l1.next.val> l2.val){
+            while (l1.next != null && l2 != null) {
+                if (l1.next.val > l2.val) {
                     ListNode nl1 = l1.next;
                     ListNode nl2 = l2.next;
-                    l1.next=l2;
-                    l2.next= nl1;
-                    l2=nl2;
-                    l1= nl1;
-                }
-                else{
+                    l1.next = l2;
+                    l2.next = nl1;
+                    l2 = nl2;
+                    l1 = nl1;
+                } else {
                     l1 = l1.next;
                 }
             }
-            if(l2!=null){
-                l1.next=l2;
+            if (l2 != null) {
+                l1.next = l2;
             }
         }
         return l1;
     }
 
+
+    static void printList(Node node) {
+        while (node != null) {
+            System.out.println(node.val);
+            node = node.next;
+        }
+    }
+
+    static public Node copyRandomList(Node head) {
+
+        Map<Node, Integer> oldNodePosition = new HashMap<>();
+        Map<Integer,Node> newNodePosition = new HashMap<>();
+        int oldNodeCounter = 0;
+        Node curNodePointer = head;
+        Node newHead = new Node(head.val);
+        Node newNodeCurr = newHead;
+        oldNodePosition.put(curNodePointer, oldNodeCounter);
+        newNodePosition.put(oldNodeCounter ,newHead);
+        curNodePointer = curNodePointer.next;
+        ++oldNodeCounter;
+
+        while (curNodePointer != null) {
+            Node newNode = new Node(curNodePointer.val);
+            newNodeCurr.next = newNode;
+            newNodeCurr = newNodeCurr.next;
+
+            oldNodePosition.put(curNodePointer, oldNodeCounter);
+            newNodePosition.put(oldNodeCounter,newNodeCurr);
+
+            curNodePointer = curNodePointer.next;
+            ++oldNodeCounter;
+        }
+
+        Node oldNodeMov = head;
+        Node newNodeMov = newHead;
+        while(oldNodeMov!=null){
+            newNodeMov.random = newNodePosition.get( oldNodePosition.get(oldNodeMov.random));
+            oldNodeMov=  oldNodeMov.next;
+            newNodeMov= newNodeMov.next;
+        }
+
+        System.out.println(oldNodePosition);
+        System.out.println(newNodePosition);
+        printList(newHead);
+
+        return newHead;
+
+    }
+
+
+    public static void main(String[] args) {
+        Node node = new Node(1);
+        node.next = new Node(2);
+        node.next.next = new Node(3);
+        copyRandomList(node);
+    }
 
 
 }
