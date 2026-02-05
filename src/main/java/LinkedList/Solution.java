e84e2dd18ffe831f19a9588a17eb8c2cbeeb0f68
dhiraj kumar <dhirajnie@gmail.com>
2026-02-05 09:44:16 +0530

    sss

--- src/main/java/LinkedList/Solution.java
+++ src/main/java/LinkedList/Solution.java
@@ -1,6 +1,8 @@
 package LinkedList;
 
 
+import jdk.nashorn.internal.ir.SplitReturn;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -39,8 +41,8 @@ class ListNode {
     }
 
     @Override
-  public  String toString(){
-        return "["+this.val+"]";
+    public String toString() {
+        return "[" + this.val + "]";
     }
 }
 
@@ -108,13 +110,13 @@ public class Solution {
     static public Node copyRandomList(Node head) {
 
         Map<Node, Integer> oldNodePosition = new HashMap<>();
-        Map<Integer,Node> newNodePosition = new HashMap<>();
+        Map<Integer, Node> newNodePosition = new HashMap<>();
         int oldNodeCounter = 0;
         Node curNodePointer = head;
         Node newHead = new Node(head.val);
         Node newNodeCurr = newHead;
         oldNodePosition.put(curNodePointer, oldNodeCounter);
-        newNodePosition.put(oldNodeCounter ,newHead);
+        newNodePosition.put(oldNodeCounter, newHead);
         curNodePointer = curNodePointer.next;
         ++oldNodeCounter;
 
@@ -124,7 +126,7 @@ public class Solution {
             newNodeCurr = newNodeCurr.next;
 
             oldNodePosition.put(curNodePointer, oldNodeCounter);
-            newNodePosition.put(oldNodeCounter,newNodeCurr);
+            newNodePosition.put(oldNodeCounter, newNodeCurr);
 
             curNodePointer = curNodePointer.next;
             ++oldNodeCounter;
@@ -132,10 +134,10 @@ public class Solution {
 
         Node oldNodeMov = head;
         Node newNodeMov = newHead;
-        while(oldNodeMov!=null){
-            newNodeMov.random = newNodePosition.get( oldNodePosition.get(oldNodeMov.random));
-            oldNodeMov=  oldNodeMov.next;
-            newNodeMov= newNodeMov.next;
+        while (oldNodeMov != null) {
+            newNodeMov.random = newNodePosition.get(oldNodePosition.get(oldNodeMov.random));
+            oldNodeMov = oldNodeMov.next;
+            newNodeMov = newNodeMov.next;
         }
 
         System.out.println(oldNodePosition);
@@ -147,18 +149,19 @@ public class Solution {
     }
 
 
-    static public List<ListNode> swap(ListNode head){
+    static public List<ListNode> swap(ListNode head,int swapCount) {
         // tail and head
 
         List<ListNode> result = new ArrayList<>();
         result.add(head);
         ListNode prev = null;
         ListNode curr = head;
-        while(curr!=null){
+        while (curr != null && swapCount>0) {
             ListNode nextTemp = curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=nextTemp;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+            --swapCount;
         }
 
 
@@ -166,29 +169,166 @@ public class Solution {
         return result;
 
     }
-// static  public ListNode reverseBetween(ListNode head, int left, int right) {
+
+    static public ListNode reverseBetween(ListNode head, int left, int right) {
+
+    //    ListNode firstIndex = null;
+        ListNode curr = head;
+        ListNode firstNode = null;
+        ListNode lastNode = null;
+
+        int counter = 1;
+
+        while (curr!=null){
+            if(counter==left-1){
+                firstNode= curr;
+            }
+            if(counter==right+1){
+                lastNode=curr;
+                break;
+            }
+            ++counter;
+            curr=curr.next;
+        }
+        if(firstNode==null){
+         List<ListNode> tailhead =    swap(head,right-left+1);
+
+         tailhead.get(0).next=lastNode;
+         return tailhead.get(1);
+
+        }
+        else{
+            List<ListNode> tailhead =  swap(firstNode.next,right-left+1);
+            firstNode.next= tailhead.get(1);
+            tailhead.get(0).next=lastNode;
+            return head;
+        }
+
+
+
+
+    }
+
+//    public ListNode deleteDuplicates(ListNode head) {
+//        ListNode prev = new ListNode(0);
+//        ListNode next= head;
 //
-//
-//
+//        while(next!=null){
+//            if(head.next!=null  && head.next.val==head.val){
+//                while()
+//            }
+//        }
 //
 //    }
+
+    public ListNode rotateRight(ListNode head, int k) {
+
+        int totalLength =0;
+        ListNode curr= head;
+        while (curr!=null){
+            ++totalLength;
+            curr=curr.next;
+        }
+
+        k = k%totalLength;
+        int cutNode = totalLength-k-1;
+        System.out.println(cutNode);
+        curr=head;
+        while(cutNode!=0){
+            --cutNode;
+            curr=curr.next;
+        }
+
+        ListNode newHead = curr.next;
+        curr.next=null;
+
+        ListNode newCurr = newHead;
+        while(newCurr.next!=null){
+            newCurr= newCurr.next;
+        }
+        newCurr.next=head;
+
+
+return newHead;
+
+    }
+
+
+
+   static public ListNode deleteDuplicates(ListNode head) {
+
+        ListNode dummy = new ListNode(0);
+        ListNode prev = dummy;
+        while(head!=null){
+            if(head.next!=null && head.val==head.next.val){
+                while(head.next!=null && head.val==head.next.val){
+                    head= head.next;
+                }
+                head = head.next;
+                prev.next=head;
+            }
+            else{
+                prev.next= head;
+                head= head.next;
+                prev= prev.next;
+            }
+
+        }
+
+        return dummy.next;
+
+    }
+
+
+
+  static   public ListNode partition(ListNode head, int x) {
+        ListNode perHead  = new ListNode(Integer.MIN_VALUE);
+        perHead.next= head;
+        head = perHead;
+        ListNode lastNode = head;
+        int nodeCount =0;
+        while(lastNode.next!=null){
+            ++nodeCount;
+            lastNode=lastNode.next;
+        }
+        ListNode mLastNode = lastNode;
+
+        while(head!=null &&nodeCount>0){
+            --nodeCount;
+            if(  head.next!=null && head.next.val>=x){
+                ListNode nextOfNext = head.next.next;
+                mLastNode.next= head.next;
+                mLastNode.next.next=null;
+                mLastNode= mLastNode.next;
+                head.next=nextOfNext;
+            }
+            else{
+                head= head.next;
+            }
+        }
+        return perHead.next;
+
+    }
+
 
 
 
 
     public static void main(String[] args) {
-        ListNode node = new ListNode(1);
-        node.next = new ListNode(2);
-        node.next.next = new ListNode(3);
-        node.next.next.next = new ListNode(4);
-      List<ListNode> tailHead =   swap(node);
+        ListNode head = new ListNode(6);
+         head.next = new ListNode(2);
+//        head.next.next = new ListNode(3);
+//        head.next.next.next = new ListNode(0);
+//        head.next.next.next.next = new ListNode(5);
+//        head.next.next.next.next.next = new ListNode(2);
 
-      ListNode newHead = tailHead.get(1);
-      while (newHead!=null){
-          System.out.println(newHead);
-          newHead= newHead.next;
-      }
-     //   System.out.println(tailHead);
+
+
+        head = partition(head,4);
+        while (head!=null){
+            System.out.print(head.val+" -> ");
+            head=head.next;
+        }
     }
 
 
