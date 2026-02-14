afa751be44a606c0310108fecd09cbe0b813e4e8
dhiraj kumar <dhirajnie@gmail.com>
2026-02-14 20:05:11 +0530

    sss

--- src/main/java/Tree/TreeTest.java
+++ src/main/java/Tree/TreeTest.java
@@ -2,6 +2,11 @@ package Tree;
 
 import com.practice.utils.Tree;
 
+import java.nio.ByteBuffer;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 class TreeNode {
       int val;
         TreeNode left;
@@ -28,8 +33,8 @@ public class TreeTest {
         if (left.val != right.val) {
             return false;
         }
-        return checkSymetic(left.left, right.right) && checkSymetic(left.right, right.left)
-    }
+        return checkSymetic(left.left, right.right) && checkSymetic(left.right, right.left);
+
     }
 
     public boolean isSymmetric(TreeNode root) {
@@ -41,7 +46,23 @@ public class TreeTest {
 
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+    String url ="abc.example.com";
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        byte[] hash = digest.digest(url.getBytes(StandardCharsets.UTF_8));
 
+// Take first 8 bytes (64 bits)
+        long value = ByteBuffer.wrap(hash).getLong();
+        System.out.println(value);
+
+        // String → bytes (encoding)
+        byte[] bytes = "abc".getBytes(StandardCharsets.UTF_8);
+
+// bytes → String (decoding)
+        String text = new String(bytes, StandardCharsets.UTF_8);
+        System.out.println(text);
+
+// Convert to Base62
+//        String shortCode = Base62.encode(Math.abs(value));
     }
 }
